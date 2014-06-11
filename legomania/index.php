745a6569a0cf16f1829f2ff82321b56261847fff
Baptiste Deleplace <baptiste.deleplace@gmail.com>
2014-06-11 21:25:51 +0200

    correction exo legomania

---
+++ legomania/index.php
@@ -0,0 +1,153 @@
+<?php
+	
+	// j'isole mon code php
+	include 'php/controller.php';
+
+?><!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+		<head>
+				<meta charset="utf-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+				<title></title>
+				<meta name="description" content="">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+
+				<link rel="stylesheet" href="css/bootstrap.min.css">
+				<style>
+						body {
+								padding-top: 50px;
+								padding-bottom: 20px;
+						}
+				</style>
+				<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+				<link rel="stylesheet" href="css/main.css">
+
+				<script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		</head>
+		<body>
+
+		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">LegoMania</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="jumbotron">
+			<div class="container">
+
+<?php /**
+	Affichage du titre
+*/ ?>
+	
+<h1><?php echo $category_title; ?></h1>
+
+				<p>Le site de tout les légos, même pas chers.</p>
+
+			</div>
+		</div>
+
+		<div class="container">
+
+			<ul class="nav nav-tabs">
+<?php /**
+	Affichage des catégories
+*/ ?>
+
+	<li <?php if($cid == 0) echo 'class="active"'; ?> >
+		<a href="?">Toutes les catégories</a>
+	</li>
+
+<?php foreach ($categories as $c): ?>
+
+	<li <?php if($c['category_id'] == $cid) echo 'class="active"'; ?> >
+		<a href="?cid=<?php echo $c['category_id']; ?>">
+			<?php echo $c['name']; ?>
+		</a>
+	</li>
+
+<?php endforeach; ?>
+
+			</ul>
+
+			<div class="row">
+
+<?php /**
+	Affichage des légos
+*/ ?>
+
+<?php if (empty($legos)): ?>
+
+	<p><i>Oups! Il n'y a pas de légo dans cette catégorie :(</i></p>
+	
+<?php else: ?>
+
+	<?php foreach ($legos as $l): ?>
+
+		<div class="col-md-3">
+			<h3><?php echo $l['name']; ?></h3>
+		</div>
+
+	<?php endforeach; ?>
+
+<?php endif; ?>
+
+
+
+			</div>
+
+
+			<hr>
+
+<?php /**
+	Affichage de la pagination
+*/ ?>
+
+<ul class="pagination">
+
+	<?php foreach ($paginations as $p): ?>
+	
+		<li>
+			<a href="<?php echo $p['uri']; ?>">
+				<?php echo $p['page_number']; ?>
+			</a>
+		</li>
+	
+	<?php endforeach ?>
+
+</ul>
+
+
+
+
+
+			<hr>
+
+			<footer>
+				<p>&copy; 3WA 2014</p>
+			</footer>
+		</div> <!-- /container -->
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+
+		<script src="js/vendor/bootstrap.min.js"></script>
+
+		<script src="js/main.js"></script>
+
+		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+		<script>
+			(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+			function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+			e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+			e.src='//www.google-analytics.com/analytics.js';
+			r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+			ga('create','UA-XXXXX-X');ga('send','pageview');
+		</script>
+	
+	</body>
+</html>
---
+++ legomania/php/controller.php
@@ -0,0 +1,152 @@
+<?php
+
+# on nettoie les variables d'input
+$cid = 0;
+if( isset($_GET['cid']) and 
+	is_numeric($_GET['cid'])
+){
+	$cid = intval($_GET['cid']);
+}
+
+$from = 0;
+if( isset($_GET['from']) and 
+	is_numeric($_GET['from'])
+){
+	$from = intval($_GET['from']);
+}
+
+
+# création d'une connection au serveur MySQL
+# voir http://www.w3schools.com/php/func_mysqli_connect.asp
+$link = mysqli_connect(
+	'localhost', 	# adresse du serveur
+	'root',			# login
+	'troiswa',		# password
+	'legomania'		# nom de la base de donnée
+);
+
+/**
+	La liste de toutes les catégories
+*/
+$categories = array();
+
+# construction de la chaine (string), qui servira de requête SQL
+$sql1 = "
+	SELECT * 
+	FROM `categorys` 
+";
+
+# envoi de cette requête à MySQL
+# voir http://www.w3schools.com/php/func_mysqli_query.asp
+$q1 = mysqli_query($link, $sql1);
+
+# on stocke dans un tableau provisoire les données
+# http://www.w3schools.com/php/func_mysqli_fetch_array.asp
+while ($data = mysqli_fetch_array($q1, MYSQLI_ASSOC)) 
+{
+	$categories[$data['category_id']] = array(
+		'category_id' => $data['category_id'],
+		'name' => ucfirst($data['name']),
+		'description' => ucfirst($data['description']),
+	);
+}
+
+/**
+	Le titre de la catégorie en cours
+*/
+
+# on met un titre par défaut ...
+$category_title = 'Bienvenue chez LegoMania !';
+
+# ... et on l'écrase si la catégorie en cours existe
+if(isset($categories[$cid])) 
+{
+	$category_title = 'Les legos ' . $categories[$cid]['name'] . ' !';
+}
+
+/**
+	Les produits de la catégorie en cours 
+*/
+$legos = array();
+
+# on créé le premier morceau de la chaine (string) 
+# de notre future requête SQL ...
+$sql2 = "
+	SELECT
+		`legos`.`name`, 
+		( 
+			( `legos`.`price` * `categorys`.`margin_rate` ) 
+			+ `categorys`.`expedition_price` 
+		) AS total_price
+	FROM  `legos`
+	LEFT JOIN `categorys` 
+		ON `categorys`.`category_id` = `legos`.`category_id`
+";
+
+# si $cid est OK, on complète cette chaîne
+if($cid){
+	$sql2 .= "
+		WHERE `categorys`.`category_id` = " . $cid . "
+	";
+}
+
+# si $from est OK, on complète cette chaîne
+$sql2 .= "
+	LIMIT " . $from . " , 3
+";
+
+# envoi de cette requête à MySQL
+$q2 = mysqli_query($link, $sql2);
+
+# on stocke dans un tableau provisoire les données
+while ($data = mysqli_fetch_array($q2, MYSQLI_ASSOC)) 
+{
+	$legos[] = array(
+		'name' => ucfirst($data['name']),
+		'price' => round($data['total_price'], 2),
+	);
+}
+
+
+/**
+	Génération de la pagination
+*/
+$paginations = array();
+
+# construction de la chaine (string), qui servira de requête SQL
+$sql3 = "
+	SELECT 
+		COUNT(*) AS n
+	FROM `legos` 
+";
+# si $cid est OK, on complète cette chaîne
+if($cid){
+	$sql3 .= "
+		WHERE `category_id` = " . $cid . "
+	";
+}
+
+# envoi de cette requête à MySQL
+$q3 = mysqli_query($link, $sql3);
+
+# on stocke dans un tableau provisoire les données
+$data = mysqli_fetch_array($q3, MYSQLI_ASSOC);
+
+# calcul du nombre de pages
+$n_legos = $data['n'];
+$n_pages = ceil( $n_legos / 3 );
+
+# on stocke dans un tableau provisoire les données utiles à la pagination
+for ($i=1; $i <= $n_pages; $i++)
+{
+	$f = ($i - 1) * 3;
+	$paginations[] = array(
+		'page_number' => $i,
+		'uri' => "?cid=" . $cid . "&from=" . $f,
+	);
+}
+# ... s'il n'y a qu'une page, on "vide" (en l'écrasant) le tableau
+if ($n_pages <= 1){
+	$paginations = array();
+}
+	
